2194845161fc05ad62f7d8a08c8650c1cd1c43d1
mason369 <1960638223@qq.com>
2022-12-31 17:06:48 +0800

    update
    Co-Authored-By: liuliu66 <101164913+liusxs@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
   <div id="app">
+	  <el-container>
+		  <el-aside width="25vw">Aside</el-aside>
+		  <el-container>
+			  <el-header>Header</el-header>
+			  <el-main>Main</el-main>
+			  <el-footer>Footer</el-footer>
+		  </el-container>
+	  </el-container>
   </div>
 </template>
 
 <script>
-
-export default {
-	name      : "App",
-}
+export default {name: "App"}
 </script>
 
-<style>
-@import './assets/scss/global.scss';
+<style scoped lang="scss">
+.el-container {
+  height: 100vh;
+	background-color: #B3C0D1;
+}
+.el-header, .el-footer {
+	height:20vh !important;
+	background-color: #B3C0D1;
+	color: #333;
+	text-align: center;
+}
+
+.el-aside {
+	background-color: #D3DCE6;
+	color: #333;
+	text-align: center;
+}
+
+.el-main {
+	background-color: #E9EEF3;
+	color: #333;
+	text-align: center;
+}
 </style>
