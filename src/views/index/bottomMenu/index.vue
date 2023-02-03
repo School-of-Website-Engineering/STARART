8363ab18ad23eba728c883ba7f1e9304b2e6f061
liusxs <1694181697@qq.com>
2023-02-03 20:23:39 +0800

    update

--- src/views/index/bottomMenu/index.vue
+++ src/views/index/bottomMenu/index.vue
@@ -14,10 +14,42 @@ export default {name: "bottomMenu"}
 
 <style lang="scss" scoped>
 @import "@/assets/scss/_media.scss";
+<<<<<<< Updated upstream
 .textOutput {
 	background: #2b579a;
 	height: $bottomMenu-height;
 	text-align: center;
+=======
+@import "@/assets/scss/_color.scss";
+
+.bottomLine {
+	border: $border solid $tip-color;
+	border-radius: $border-radius;
+	width: $body-width;
+	height: 6vh;
+	margin: 0 auto;
+	display: flex;
+	justify-content: space-around;
+	margin-top: 1px;
+	.time-list{
+		width: 1.5rem;
+		font-size: .1rem;
+		margin: auto;
+		text-align: center;
+		border: $border solid $tip-color;
+		border-radius: 5px;
+		padding:0.1rem;
+	}
+	.pause{
+		width: 2rem;
+		font-size: .1rem;
+		margin: auto;
+		text-align: center;
+		border: $border solid $tip-color;
+		border-radius: 5px;
+		padding:0.1rem;
+	}
+>>>>>>> Stashed changes
 }
 
 .more {
--- src/views/index/event/index.vue
+++ src/views/index/event/index.vue
@@ -1,8 +1,34 @@
 <template>
 	<div>
 		<van-row type="flex" class="eventMenu">
+<<<<<<< Updated upstream
 			<van-col span="8" class="text">快捷栏</van-col>
 			<van-col span="16" class="event">文字栏</van-col>
+=======
+			<van-col span="8" class="text">
+				<div class="events-head">地图</div>
+			</van-col>
+			<van-col span="16" class="event">
+				<div class="events-head">事件区
+				</div>
+			<div class="mainflex">
+				<!-- 主菜单界面 -->
+				<div class="menu-main">
+					<!-- TODO 事件区 -->
+					<div class="menu-list">
+						制作
+					</div>
+					<div class="menu-list">
+						天赋
+					</div>
+					<div class="menu-list">
+						休息
+					</div>
+				</div>
+				
+			</div>
+			</van-col>
+>>>>>>> Stashed changes
 		</van-row>
 	</div>
 </template>
@@ -13,6 +39,7 @@ export default {name: "event"}
 
 <style lang="scss" scoped>
 @import "@/assets/scss/_media.scss";
+<<<<<<< Updated upstream
 .event{
 	background: cadetblue;
 	height: $event-height;
@@ -22,5 +49,57 @@ export default {name: "event"}
 	background: rgb(160, 95, 95);
 	height: $event-height;
 	text-align: center;
+=======
+@import "@/assets/scss/_color.scss";
+//菜单模板
+.menu-main{
+	float: right;
+}
+.menu-list{
+	width: .8rem;
+	font-size: .1rem;
+	text-align: center;
+	border: $border solid $tip-color;
+	border-radius: 5px;
+	margin-top: .2rem;
+
+}
+.eventMenu{
+	border: $border solid $tip-color;
+	border-radius: $border-radius;
+	width: $body-width;
+	height: $eventMenu-height;
+	margin: 0 auto;
+	display: flex;
+	justify-content: space-around;
+	margin-top: .125rem;
+	.text{
+	    height: $event-height;
+		border: $border solid $tip-color;
+		border-radius: $border-radius;
+		position: relative;
+		width: 40%;
+		margin-top: .15rem;
+    }
+	.event{
+	    height: $event-height;
+		border: $border solid $tip-color;
+		border-radius: $border-radius;
+		position: relative;
+		width: 50%;
+		margin-top: .15rem;
+    }
+	.events-head{
+		    width: 1.5rem;
+		    font-size: .1rem;
+		    text-align: center;
+		    border: $border solid $tip-color;
+	        border-radius: 5px;
+		    position: absolute;
+		    top: -3px;
+		    left: 6px;
+			background-color: #fff;
+		}
+>>>>>>> Stashed changes
 }
 </style>
