15462d64680e3be01de06ca0fc22e13a13c81fbb
liusxs <1694181697@qq.com>
2023-02-06 16:43:19 +0800

    布局

--- src/views/index/bottomMenu/index.vue
+++ src/views/index/bottomMenu/index.vue
@@ -3,9 +3,25 @@
 		<van-row type="flex" justify="space-around" class="bottomMenu">
 			<van-col span="12" class="textOutput">
 				<div class="menu-head">文本输出</div>
+				<div class="menu-main">
+					<h3>你失去了两份水</h3>
+				</div>
 			</van-col>
 			<van-col span="12" class="more">
 				<div class="menu-head">更多</div>
+				<div class="menu-content">
+					<div class="content-btn">
+						<van-button plain hairline type="danger" size="mini">读取存档</van-button>
+						<van-button plain hairline type="danger" size="mini">世界图鉴</van-button>
+					</div>
+					<div class="content-etn">
+						<van-button plain hairline type="danger" size="mini">包</van-button>
+					</div>
+					<div class="content-btn">
+						<van-button plain hairline type="danger" size="mini">探索笔记</van-button>
+						<van-button plain hairline type="danger" size="mini">关于我们</van-button>
+					</div>
+				</div>
 			</van-col>
 		</van-row>
 		<van-row type="flex" justify="space-around" class="bottomLine">
@@ -21,7 +37,7 @@
 </template>
 
 <script>
-import {mapGetters, mapActions} from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
 	name: "bottomMenu",
@@ -29,8 +45,8 @@ export default {
 		// 获取timer的游戏世界时间(changeTime)
 		this.changeTime();
 	},
-	computed: {...mapGetters("timer", ["worldTime"])},
-	methods : {...mapActions("timer", ["changeTime"])}
+	computed: { ...mapGetters("timer", ["worldTime"]) },
+	methods: { ...mapActions("timer", ["changeTime"]) }
 }
 </script>
 
@@ -45,7 +61,7 @@ export default {
 	margin-top: 1px;
 	border: $border solid $tip-color;
 	border-radius: 5px;
-	
+
 	.time-list {
 		width: 1.5rem;
 		font-size: .1rem;
@@ -54,7 +70,7 @@ export default {
 		border: $border solid $tip-color;
 		border-radius: 5px;
 	}
-	
+
 	.pause {
 		width: 2rem;
 		font-size: .1rem;
@@ -73,7 +89,7 @@ export default {
 	display: flex;
 	justify-content: space-around;
 	margin: .125rem auto 0;
-	
+
 	.textOutput {
 		height: $Menu-height;
 		border: $border solid $tip-color;
@@ -82,7 +98,7 @@ export default {
 		width: 40%;
 		margin-top: .15rem;
 	}
-	
+
 	.more {
 		height: $Menu-height;
 		border: $border solid $tip-color;
@@ -91,7 +107,7 @@ export default {
 		width: 50%;
 		margin-top: .15rem;
 	}
-	
+
 	.menu-head {
 		width: 1.5rem;
 		font-size: .1rem;
@@ -102,6 +118,29 @@ export default {
 		top: -3px;
 		left: 6px;
 		background-color: #fff;
+	}
+
+	.menu-main {
+		font-size: .1rem;
+		margin: 15px;
+	}
+
+	.menu-content {
+		width: 90%;
+		margin: 0 auto;
+		margin-top: 25px;
+
+		.content-btn {
+			margin: 5px;
+			display: flex;
+			justify-content: space-between;
+		}
+
+		.content-etn {
+			position: absolute;
+			left: 72px;
+			top: 26px;
+		}
 	}
 }
 </style>
