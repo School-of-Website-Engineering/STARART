f86a734d766de384fa77afeedf238daa39005bab
mason369 <1960638223@qq.com>
2023-02-07 09:56:00 +0800

    完成50%布局

--- src/views/index/event/index.vue
+++ src/views/index/event/index.vue
@@ -2,27 +2,16 @@
 	<div class="eventMenuBox">
 		<van-row type="flex" class="eventMenu">
 			<van-col span="8" class="text">
-				<div class="events-head">地图</div>
+				<van-tag class="events-head" color="#7232dd">地图</van-tag>
 			</van-col>
 			<van-col span="16" class="event">
-				<div class="events-head">事件区
-				</div>
-			<div class="mainflex">
-				<!-- 主菜单界面 -->
-				<div class="menu-main">
+				<van-tag class="events-head" color="#7232dd">事件</van-tag>
+				<div class="mainflex">
 					<!-- TODO 事件区 -->
-					<div class="menu-list">
-						制作
-					</div>
-					<div class="menu-list">
-						天赋
-					</div>
-					<div class="menu-list">
-						休息
-					</div>
+					<van-tag class="menu-list" type="primary" plain size="large">制作</van-tag>
+					<van-tag class="menu-list" plain type="primary" size="large">天赋</van-tag>
+					<van-tag plain class="menu-list" type="primary" size="large">休息</van-tag>
 				</div>
-				
-			</div>
 			</van-col>
 		</van-row>
 	</div>
@@ -35,21 +24,30 @@ export default {name: "event"}
 <style lang="scss" scoped>
 @import "@/assets/scss/_media.scss";
 @import "@/assets/scss/_color.scss";
-//菜单模板
-.menu-main{
-	float: right;
-}
-.menu-list{
-	width: .8rem;
-	font-size: .1rem;
-	text-align: center;
-	border: $border solid $tip-color;
-	border-radius: 5px;
-	margin-top: .2rem;
 
+.mainflex {
+	position: relative;
+	left: 390px;
+	width: 40px;
+	display: flex;
+	flex-wrap: wrap;
+	margin: 0;
 }
-.eventMenu{
-	border: $border solid $tip-color;
+
+.menu-list {
+	width: 18px;
+	height: 48px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: $border solid;
+	border-radius: 5px;
+	margin-top: 10px;
+	padding: 8px;
+}
+
+.eventMenu {
+	border: $border solid #7232dd;
 	border-radius: $border-radius;
 	width: $body-width;
 	height: $eventMenu-height;
@@ -57,32 +55,36 @@ export default {name: "event"}
 	justify-content: space-around;
 	margin: .125rem auto 0;
 	
-	.text{
-	    height: $event-height;
-		border: $border solid $tip-color;
+	.text {
+		height: $event-height;
+		border: $border solid #7232dd;
 		border-radius: $border-radius;
 		position: relative;
 		width: 38%;
 		margin-top: .15rem;
-    }
-	.event{
-	    height: $event-height;
-		border: $border solid $tip-color;
+	}
+	
+	.event {
+		height: $event-height;
+		border: $border solid #7232dd;
 		border-radius: $border-radius;
 		position: relative;
 		width: 59%;
 		margin-top: .15rem;
-    }
-	.events-head{
-		    width: 1.5rem;
-		    font-size: .1rem;
-		    text-align: center;
-		    border: $border solid $tip-color;
-	        border-radius: 5px;
-		    position: absolute;
-		    top: -3px;
-		    left: 6px;
-			background-color: #fff;
-		}
+	}
+	
+	.events-head {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 59px;
+		height: 40px;
+		text-align: center;
+		border-radius: 5px;
+		position: absolute;
+		top: -3px;
+		left: 6px;
+		background-color: #fff;
+	}
 }
 </style>
