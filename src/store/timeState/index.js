111ddc35c6f2d8ef99755b6b9abe5955faefacea
mason369 <1960638223@qq.com>
2023-02-09 19:32:45 +0800

    添加当前时间状态

--- src/store/timeState/index.js
+++ src/store/timeState/index.js
@@ -1,12 +1,10 @@
 //时间当前情景状态
-//引入timer
-import timer from "@/store/timer";
 
 export default {
 	namespaced: true,
 	// 提交 mutations 触发修改
 	actions   : {
-		setCurrentTime({commit}, payload) {
+		setCurrentTime({ commit }, payload) {
 			commit("SET_CURRENT_TIME", payload);
 		}
 	},
@@ -15,36 +13,49 @@ export default {
 		SET_CURRENT_TIME(state, payload) {
 			state.currentTime = payload;
 			// 根据当前时间计算世界状态 , 怪物密度 和 生物密度,
-			// 将时间转化成以分钟为单位
-			const timeInMinutes
-				= Math.floor(state.currentTime / 100) * 60
-				+ (state.currentTime % 100);
-			if (timeInMinutes >= 0 && timeInMinutes <= 459) {
+			// 时间	世界状态	怪物密度值	生物密度值
+			// 00:00-04:59	凌晨深夜	4	1
+			// 05:00-07:59	黎明到来	3	2
+			// 08:00-11:59	早日清晨	2	3
+			// 12:00-16:59	晌午时分	1	5
+			// 17:00-19:59	傍晚降至	3	2
+			// 20:00-23:59	夜幕降临	5	1
+
+			//转化时间为小时与分钟(0:00-23:59)分割":"
+			let time = state.currentTime.split(":");
+			//转化为小时与分钟(0-23)(0-59)
+			let hour = parseInt(time[0]);
+			//parseInt() 函数可解析一个字符串，并返回一个整数。
+			let minute = parseInt(time[1]);
+			//计算当前时间的分钟数
+			let minuteCount = hour * 60 + minute;
+			//根据当前时间的分钟数计算世界状态 , 怪物密度 和 生物密度
+			if (minuteCount >= 0 && minuteCount < 300) {
 				state.worldStatus = "凌晨深夜";
 				state.monsterDensity = 4;
 				state.creatureDensity = 1;
 			}
-			else if (timeInMinutes >= 500 && timeInMinutes <= 759) {
+			else if (minuteCount >= 300 && minuteCount < 480) {
 				state.worldStatus = "黎明到来";
 				state.monsterDensity = 3;
 				state.creatureDensity = 2;
 			}
-			else if (timeInMinutes >= 800 && timeInMinutes <= 1159) {
+			else if (minuteCount >= 480 && minuteCount < 720) {
 				state.worldStatus = "早日清晨";
 				state.monsterDensity = 2;
 				state.creatureDensity = 3;
 			}
-			else if (timeInMinutes >= 1200 && timeInMinutes <= 1659) {
+			else if (minuteCount >= 720 && minuteCount < 1020) {
 				state.worldStatus = "晌午时分";
 				state.monsterDensity = 1;
 				state.creatureDensity = 5;
 			}
-			else if (timeInMinutes >= 1700 && timeInMinutes <= 1959) {
+			else if (minuteCount >= 1020 && minuteCount < 1200) {
 				state.worldStatus = "傍晚降至";
 				state.monsterDensity = 3;
 				state.creatureDensity = 2;
 			}
-			else if (timeInMinutes >= 2000 && timeInMinutes <= 2359) {
+			else if (minuteCount >= 1200 && minuteCount < 1440) {
 				state.worldStatus = "夜幕降临";
 				state.monsterDensity = 5;
 				state.creatureDensity = 1;
@@ -55,7 +66,7 @@ export default {
 	state: {
 		// 当前游戏时间
 		//载入timer游戏时间
-		currentTime    : timer.state.hours * 100 + timer.state.minutes,
+		currentTime    : 0,
 		// 当前世界状态
 		worldStatus    : "",
 		// 当前怪物密度
